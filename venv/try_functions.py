b73a6e3c6826ae115d54af88fbc67ebfa2c937b8
Abed <freazer122@gmail.com>
2021-10-28 12:44:02 +0300

    lets see

--- venv/try_functions.py
+++ venv/try_functions.py
@@ -3,35 +3,35 @@ from hw1_functions import *
 
 if __name__ == "__main__":
     # feel free to add/remove/edit lines
-    path_image = r'C:\Users\Samer\OneDrive - University of Haifa\university-samer\image_Proccessing\hw1\Images\darkimage.tif'
+    path_image = r'C:\Users\freaz\Desktop\image processing\abed\Images\darkimage.tif'
     darkimg = cv2.imread(path_image)
     darkimg_gray = cv2.cvtColor(darkimg, cv2.COLOR_BGR2GRAY)
 
-    path_image2 = r'C:\Users\Samer\OneDrive - University of Haifa\university-samer\image_Proccessing\hw1\Images\fruit.tif'
+    path_image2 = r'C:\Users\freaz\Desktop\image processing\abed\Images\fruit.tif'
     darkimg2 = cv2.imread(path_image2)
     darkimg_gray2 = cv2.cvtColor(darkimg2, cv2.COLOR_BGR2GRAY)
 
-    # print("a ------------------------------------\n")
-    # # contrastEnhance(darkimg_gray, range=[0, 255])
-    # range = [np.amin(darkimg_gray),np.amax(darkimg_gray)]
-    # enhanced_img, a, b = contrastEnhance(darkimg_gray,range)  # add parameters
+    print("a ------------------------------------\n")
+    # contrastEnhance(darkimg_gray, range=[0, 255])
+    range = [np.amin(darkimg_gray),np.amax(darkimg_gray)]
+    enhanced_img, a, b = contrastEnhance(darkimg_gray,range)  # add parameters
+#let see what happens
 
+    #display images
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(darkimg)
+    plt.title('original')
 
-    # display images
-    # plt.figure()
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(darkimg)
-    # plt.title('original')
-    #
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(enhanced_img, cmap='gray')
-    # plt.title('enhanced contrast')
-    #
-    # # print a,b
-    # print("a = {}, b = {}\n".format(a, b))
-    #
-    # # display mapping
-    # showMapping(range,a,b)  # add parameters
+    plt.subplot(1, 2, 2)
+    plt.imshow(enhanced_img, cmap='gray')
+    plt.title('enhanced contrast')
+
+    # print a,b
+    print("a = {}, b = {}\n".format(a, b))
+
+    # display mapping
+    showMapping(range,a,b)  # add parameters
 
     # print("b ------------------------------------\n")
     # enhanced2_img, a, b = contrastEnhance(path_image, range)  # add parameters
