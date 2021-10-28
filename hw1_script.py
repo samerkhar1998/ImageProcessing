2eb81bc69a0e3b770999ee0a9a60748cba4fda81
samer <khsamer11@gmail.com>
2021-10-28 23:05:15 +0300

    need to implement:sltNegative and sltThreshold.

--- hw1_script.py
+++ hw1_script.py
@@ -1,125 +1,148 @@
+import cv2.cv2
 import numpy as np
+
 from hw1_functions import *
 
 if __name__ == "__main__":
     # feel free to add/remove/edit lines
 
+
     path_image = r'C:\Users\Samer\OneDrive - University of Haifa\university-samer\image_Proccessing\hw1\Images\darkimage.tif'
     darkimg = cv2.imread(path_image)
     darkimg_gray = cv2.cvtColor(darkimg, cv2.COLOR_BGR2GRAY)
 
-    path_image2 = r'C:\Users\Samer\OneDrive - University of Haifa\university-samer\image_Proccessing\hw1\Images\fruit.tif'
+    path_image2 = r'C:\Users\Samer\OneDrive - University of Haifa\university-samer\image_Proccessing\hw1\Images\RealLena.tif'
     darkimg2 = cv2.imread(path_image2)
     darkimg_gray2 = cv2.cvtColor(darkimg2, cv2.COLOR_BGR2GRAY)
 
     print("Start running script  ------------------------------------\n")
     print_IDs()
 
-    print("a ------------------------------------\n")
-    range = [np.amin(darkimg_gray), np.amax(darkimg_gray)]
-    enhanced_img, a, b = contrastEnhance(darkimg_gray,range)  # add parameters
+    # print("a ------------------------------------\n")
+    # range = [100, 150]
+    # enhanced_img, a, b = contrastEnhance(darkimg_gray,range)  # add parameters
+    #
+    # # display images
+    # plt.figure()
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(darkimg)
+    # plt.title('original')
+    #
+    # plt.subplot(1, 2, 2)
+    # plt.imshow(enhanced_img, cmap='gray', vmin=0, vmax=255)
+    # plt.title('enhanced contrast')
+    #
+    # # print a,b
+    # print("a = {}, b = {}\n".format(a, b))
+    #
+    # # display mapping
+    # showMapping(range,a,b)  # add parameters
+    #
+    # print("b ------------------------------------\n")
+    # enhanced2_img, a, b = contrastEnhance(enhanced_img,[0,255])  # add parameters
+    # # enhanced2_img = cv2.cv2.cvtColor(enhanced2_img,cv2.cv2.COLOR_BGR2GRAY)
+    # # print a,b
+    # print("enhancing an already enhanced image\n")
+    # print("a = {}, b = {}\n".format(a, b))
+    #
+    # # TODO: display the difference between the two image (Do not simply display both images)--done!
+    #
+    # plt.figure()
+    # plt.subplot(1, 2, 1)
+    # difference = enhanced2_img - enhanced_img
+    # plt.imshow(enhanced2_img, cmap='gray', vmin=0, vmax=255)
+    # plt.title('max contrast enhance')
+    #
+    # plt.subplot(1, 2, 2)
+    # difference = enhanced2_img - enhanced_img
+    # plt.imshow(difference,cmap='gray', vmin=0, vmax=255)
+    # plt.title('Diff between enhanced1 and max enhance')
 
-    # display images
-    plt.figure()
-    plt.subplot(1, 2, 1)
-    plt.imshow(darkimg)
-    plt.title('original')
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(enhanced_img, cmap='gray', vmin=0, vmax=255)
-    plt.title('enhanced contrast')
-
-    # print a,b
-    print("a = {}, b = {}\n".format(a, b))
-
-    # display mapping
-    showMapping(range,a,b)  # add parameters
-
-    print("b ------------------------------------\n")
-    enhanced2_img, a, b = contrastEnhance()  # add parameters
-    # print a,b
-    print("enhancing an already enhanced image\n")
-    print("a = {}, b = {}\n".format(a, b))
-
-    # TODO: display the difference between the two image (Do not simply display both images)
-
-    print("c ------------------------------------\n")
-    mdist = minkowski2Dist(darkimg_gray,darkimg_gray2)  # add parameters
-    print("Minkowski dist between image and itself\n")
-    print("d = {}\n".format(mdist))
+    # print("c ------------------------------------\n")
+    # mdist = minkowski2Dist(darkimg_gray,darkimg_gray)  # add parameters
+    # print("Minkowski dist between image and itself\n")
+    # print("d = {}\n".format(mdist))
 
     # TODO:
     # implement the loop that calculates minkowski distance as function of increasing contrast
+    # max = np.amax(darkimg_gray)
+    # min = np.amin(darkimg_gray)
+    # step = (max - min)/20
+    # contrast = []
+    # dists = []
+    # for i in range(1,21):
+    #     newIm, _, _a= contrastEnhance(darkimg_gray, [min, min + i * step])
+    #     contrast.append(i*step)
+    #     dists.append(minkowski2Dist(darkimg_gray, newIm))
+    #
+    #
+    # plt.figure()
+    # plt.plot(contrast, dists)
+    # plt.xlabel("contrast")
+    # plt.ylabel("distance")
+    # plt.title("Minkowski distance as function of contrast")
 
-    contrast =  # TODO
-    dists =  # TODO
+    # print("d ------------------------------------\n")
+    #
+    # d = np.sum(np.ravel(darkimg_gray) - np.matmul(sliceMat(darkimg_gray),np.arange(0,256)))# computationally prove that sliceMat(im) * [0:255] == im
+    # print("{}".format(d))
+    #
+    # print("e ------------------------------------\n")
+    #
+    # d = np.sum(np.ravel(darkimg_gray) - np.matmul(sliceMat(darkimg_gray2),np.arange(0,256))) # computationally compare
+    # print("sum of diff between image and slices*[0..255] = {}".format(d))
+    #
+    # # then display
+    # plt.figure()
+    # plt.subplot(1, 2, 1)
+    # plt.imshow(darkimg)
+    # plt.title("original image")
+    # plt.subplot(1, 2, 2)
+    # TMim, TM = SLTmap(darkimg_gray, darkimg_gray2)
+    # plt.imshow(TMim, cmap='gray', vmin=0, vmax=255)
+    # plt.title("tone mapped")
 
-    plt.figure()
-    plt.plot(contrast, dists)
-    plt.xlabel("contrast")
-    plt.ylabel("distance")
-    plt.title("Minkowski distance as function of contrast")
+    # print("f ------------------------------------\n")
+    # negative_im = sltNegative(darkimg_gray)
+    # plt.figure()
+    # plt.imshow(negative_im, cmap='gray', vmin=0, vmax=255)
+    # plt.title("negative image using SLT")
 
-    print("d ------------------------------------\n")
-
-    d =  # computationally prove that sliceMat(im) * [0:255] == im
-    print("".format(d))
-
-    print("e ------------------------------------\n")
-
-    d =  # computationally compare
-    print("sum of diff between image and slices*[0..255] = {}".format(d))
-
-    # then display
-    plt.figure()
-    plt.subplot(1, 2, 1)
-    plt.imshow(darkimg)
-    plt.title("original image")
-    plt.subplot(1, 2, 2)
-    plt.imshow(TMim, cmap='gray', vmin=0, vmax=255)
-    plt.title("tone mapped")
-
-    print("f ------------------------------------\n")
-    negative_im = sltNegative(darkimg_gray)
-    plt.figure()
-    plt.imshow(negative_im, cmap='gray', vmin=0, vmax=255)
-    plt.title("negative image using SLT")
-
-    print("g ------------------------------------\n")
-    thresh = 120  # play with it to see changes
-    lena = cv2.imread(r"Images\\RealLena.tif")
-    lena_gray = cv2.cvtColor(lena, cv2.COLOR_BGR2GRAY)
-    thresh_im = sltThreshold()  # add parameters
-
-    plt.figure()
-    plt.imshow(thresh_im, cmap='gray', vmin=0, vmax=255)
-    plt.title("thresh image using SLT")
-
-    print("h ------------------------------------\n")
-    im1 = lena_gray
-    im2 = darkimage
-    SLTim =  # TODO
-
-    # then print
-    plt.figure()
-    plt.subplot(1, 3, 1)
-    plt.imshow(im1)
-    plt.title("original image")
-    plt.subplot(1, 3, 2)
-    plt.imshow(SLTim, cmap='gray', vmin=0, vmax=255)
-    plt.title("tone mapped")
-    plt.subplot(1, 3, 3)
-    plt.imshow(im2, cmap='gray', vmin=0, vmax=255)
-    plt.title("tone mapped")
-
-    d1 =  # mean sqr dist between im1 and im2
-    d2 =  # mean sqr dist between mapped image and im2
-    print("mean sqr dist between im1 and im2 = {}\n".format(d1))
-    print("mean sqr dist between mapped image and im2 = {}\n".format(d2))
-
-    print("i ------------------------------------\n")
-    # prove comutationally
-    d =  # TODO:
-    print(" {}".format(d))
+    # print("g ------------------------------------\n")
+    # thresh = 120  # play with it to see changes
+    # lena = cv2.imread(r"Images\\RealLena.tif")
+    # lena_gray = cv2.cvtColor(lena, cv2.COLOR_BGR2GRAY)
+    # thresh_im = sltThreshold()  # add parameters
+    #
+    # plt.figure()
+    # plt.imshow(thresh_im, cmap='gray', vmin=0, vmax=255)
+    # plt.title("thresh image using SLT")
+    #
+    # print("h ------------------------------------\n")
+    # im1 = lena_gray
+    # im2 = darkimage
+    # SLTim =  # TODO
+    #
+    # # then print
+    # plt.figure()
+    # plt.subplot(1, 3, 1)
+    # plt.imshow(im1)
+    # plt.title("original image")
+    # plt.subplot(1, 3, 2)
+    # plt.imshow(SLTim, cmap='gray', vmin=0, vmax=255)
+    # plt.title("tone mapped")
+    # plt.subplot(1, 3, 3)
+    # plt.imshow(im2, cmap='gray', vmin=0, vmax=255)
+    # plt.title("tone mapped")
+    #
+    # d1 =  # mean sqr dist between im1 and im2
+    # d2 =  # mean sqr dist between mapped image and im2
+    # print("mean sqr dist between im1 and im2 = {}\n".format(d1))
+    # print("mean sqr dist between mapped image and im2 = {}\n".format(d2))
+    #
+    # print("i ------------------------------------\n")
+    # # prove comutationally
+    # d =  # TODO:
+    # print(" {}".format(d))
 
     plt.show()
--- venv/try_functions.py
+++ venv/try_functions.py
@@ -1,8 +1,10 @@
+import cv2.cv2
 import numpy as np
 from hw1_functions import *
 
 if __name__ == "__main__":
     # feel free to add/remove/edit lines
+
     path_image = r'C:\Users\Samer\OneDrive - University of Haifa\university-samer\image_Proccessing\hw1\Images\lena.tif'
     darkimg = cv2.imread(path_image)
     darkimg_gray = cv2.cvtColor(darkimg, cv2.COLOR_BGR2GRAY)
@@ -12,44 +14,48 @@ if __name__ == "__main__":
     darkimg_gray2 = cv2.cvtColor(darkimg2, cv2.COLOR_BGR2GRAY)
 
     # print("a ------------------------------------\n")
-    # contrastEnhance(darkimg_gray, range=[0, 255])
-    # range = [np.amin(darkimg_gray),np.amax(darkimg_gray)]
-    # enhanced_img, a, b = contrastEnhance(darkimg_gray,range)  # add parameters
-
-
-    # display images
+    #
+    # Range = [0,250]
+    # enhanced_img, a, b = contrastEnhance(darkimg,Range)  # add parameters
+    #
+    # # display images
     # plt.figure()
     # plt.subplot(1, 2, 1)
     # plt.imshow(darkimg)
     # plt.title('original')
-
+    #
     # plt.subplot(1, 2, 2)
-    # plt.imshow(enhanced_img, cmap='gray')
+    # plt.imshow(enhanced_img, cmap='gray', vmin=0, vmax=255)
     # plt.title('enhanced contrast')
-
-    # print a,b
+    #
+    # # print a,b
     # print("a = {}, b = {}\n".format(a, b))
     #
     # # display mapping
-    # showMapping(range,a,b)  # add parameters
-
+    # showMapping(Range,a,b)  # add parameters
+    #
     # print("b ------------------------------------\n")
-    # enhanced2_img, a, b = contrastEnhance(path_image, range)  # add parameters
+    # enhanced2_img, a, b = contrastEnhance(enhanced_img, [100,255])  # add parameters
+    #
     # # print a,b
     # print("enhancing an already enhanced image\n")
     # print("a = {}, b = {}\n".format(a, b))
+    # plt.figure()
+    # plt.subplot(1, 1, 1)
+    # plt.imshow(enhanced2_img,cmap='gray')
+    # plt.title('enhanced again')
 
     # print("c ------------------------------------\n")
-    # mdist = minkowski2Dist(darkimg_gray,darkimg_gray2)  # add parameters
+    # mdist = minkowski2Dist(darkimg_gray,darkimg_gray)  # add parameters
     # print("Minkowski dist between image and itself\n")
     # print("d = {}\n".format(mdist))
-    #
+
     # print("c ------------------------------------\n")
     #
     # d = meanSqrDist(darkimg_gray,darkimg_gray2)  # add parameters
     # print("mean sqr dist between im1 and im2 = {}\n".format(d))
 
-    # SL = sliceMat(darkimg_gray)
+
 
     # print("d ------------------------------------\n")
     #
@@ -70,11 +76,11 @@ if __name__ == "__main__":
     # plt.subplot(1, 2, 2)
     # plt.imshow(TMim, cmap='gray', vmin=0, vmax=255)
     # plt.title("tone mapped")
-    print("f ------------------------------------\n")
-    negative_im = sltNegative(darkimg_gray)
-    plt.figure()
-    plt.imshow(negative_im, cmap='gray', vmin=0, vmax=255)
-    plt.title("negative image using SLT")
+    # print("f ------------------------------------\n")
+    # negative_im = sltNegative(darkimg_gray)
+    # plt.figure()
+    # plt.imshow(negative_im, cmap='gray', vmin=0, vmax=255)
+    # plt.title("negative image using SLT")
 
 
 
