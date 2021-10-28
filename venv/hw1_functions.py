8430b93f36e50dc98bf836e61f2da59c05b3312a
Abed <freazer122@gmail.com>
2021-10-28 16:29:46 +0300

    lets see

--- venv/hw1_functions.py
+++ venv/hw1_functions.py
@@ -8,7 +8,6 @@ import math
 def print_IDs():
     print("209050202+208517631\n")
 
-
 def contrastEnhance(im,range):
     # new(x,y) = a (old(xy))-> we want new(a,y) to be 255 -> a = new(x,y)/old(x,y) -> a = 255 / (y-x)
     # y=ax+b -> y=0(to calculate b in linear fun) -> in case y=0 wich means new(x,y) = y sow we know that old(x,y) is the old minimum gray
@@ -21,7 +20,6 @@ def contrastEnhance(im,range):
 
     return nim, a,b
 
-
 def showMapping(old_range, a, b):
     imMin = np.min(old_range)
     imMax = np.max(old_range)
@@ -32,7 +30,6 @@ def showMapping(old_range, a, b):
     plt.xlim([0, 255])
     plt.ylim([0, 255])
     plt.title('contrast enhance mapping')
-
 
 def minkowski2Dist(im1,im2):# p=2 -> Euclidean distance
 
@@ -51,14 +48,12 @@ def minkowski2Dist(im1,im2):# p=2 -> Euclidean distance
     #    (that is not a typo — the ending range of the cv2.calcHist function is non-inclusive so you’ll want to provide a value of 256 rather than 255) for each channel,
     #    but if you are using a color space other than RGB [such as HSV], the ranges might be different.)
 
-    return math.sqrt(np.sum((np.square(hist1- hist2))))#first distance between two pix then do square for each element in the new hist
-                                                       #then sum all the elements all together then return the sqr for the sum.
-
+    return math.sqrt(np.sum((np.square(hist1- hist2))))# first distance between two pix then do square for each element in the new hist
+                                                       # then sum all the elements all together then return the sqr for the sum.
 
 def meanSqrDist(im1, im2):
 
     return np.mean(np.square(np.array(im1) - np.array(im2)))
-
 
 def sliceMat(im):
 
@@ -72,22 +67,35 @@ def sliceMat(im):
 
     return np.transpose(SL)
 
-
 def SLTmap(im1, im2):
-    # TODO: implement fucntion
+    SLim1 = np.transpose(sliceMat(im1))
+    TM = np.arange(256,dtype=float)
+
+    for i in range(0,256):
+        if len(np.ravel(im2)[np.where(SLim1[i] == True)]) != 0 :
+            TM[i] = np.average(np.ravel(im2)[np.where(SLim1[i] == True)])
+
     return mapImage(im1, TM), TM
 
-
 def mapImage (im,tm):
-    # TODO: implement fucntion
-    return TMim
+    TMim = im
 
+    for i in range(0,256):
+        np.ravel(TMim)[np.ravel(im) == i] = tm[i]
+
+    return  TMim
 
 def sltNegative(im):
-	# TODO: implement fucntion - one line
-    return
+    sliced = np.transpose(sliceMat(im))
+    lenofim=len(im)*len(im[0])
+    tm = np.arange(lenofim, dtype=float)
+    for i in range(lenofim):
+        if len(np.ravel(im)[np.where(sliced[i] == True)]) != 0:
+            tm[i] = 255-np.ravel(im)[np.where(sliced[i] == True)]
+    nim = mapImage(im,tm)
+
+    return nim
 
 
 def sltThreshold(im, thresh):
-    # TODO: implement fucntion
     return
